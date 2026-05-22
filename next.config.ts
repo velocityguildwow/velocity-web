90da3b411a09bf9de04fdb9b83532012268af786
Rav <arkincodes@gmail.com>
2026-05-22 12:44:27 -0400

    Fix hopefully

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  serverExternalPackages: ["postgres", "drizzle-orm", "@ravxd/velocitydb"],
+};
 
 export default nextConfig;
